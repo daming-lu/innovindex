a2b52a21214fb7c00fed922b864ab6d5d5525bac
daminglu <daminglu@yahoo.com>
2017-09-25 04:18:07 -0700

    try AD -

--- pubmed/crawler_medline.py
+++ pubmed/crawler_medline.py
@@ -1,6 +1,8 @@
 import pymysql.cursors
 import urllib
 import json
+from unidecode import unidecode
+from Bio import Medline
 
 url_prefix = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term='
 
@@ -12,32 +14,78 @@ connection = pymysql.connect(host='localhost',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
+import linecache
+import sys
+
+def PrintException():
+    exc_type, exc_obj, tb = sys.exc_info()
+    f = tb.tb_frame
+    lineno = tb.tb_lineno
+    filename = f.f_code.co_filename
+    linecache.checkcache(filename)
+    line = linecache.getline(filename, lineno, f.f_globals)
+    print 'EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj)
+
 
 def insert_to_db(row):
+    for k in row:
+        row[k] = row[k].encode("utf-8")
     try:
         with connection.cursor() as cursor:
             # Create a new record
-            sql = "INSERT INTO `medline` (`uid`, `doi`, `category`, `pubdate`, `epubdate`, `source`, `authors`, `lastauthor`, " + \
+            sql = "INSERT INTO `medline_with_location_and_abstract` (`uid`, `doi`, `category`, `pubdate`, `epubdate`, `source`, `authors`, `lastauthor`, " + \
                     "`title`, `sorttitle`, `volume`, `issue`, `pages`, `lang`, `nlmuniqueid`, `issn`, `essn`, " + \
                     "`pubtype`, `recordstatus`, `pubstatus`,  `fulljournalname`, `sortfirstauthor`, `sortpubdate`, " + \
-                    "`vernaculartitle`) " + \
-                    "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                    "`vernaculartitle`, `medline_ad`, `abstract`) " + \
+                    "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
 
             sql_data = ( row['uid'], row['doi'], row['category'], row['pubdate'], row['epubdate'], row['source'], row['authors'], \
                          row['lastauthor'], row['title'], row['sorttitle'], row['volume'], row['issue'],\
                          row['pages'], row['lang'], row['nlmuniqueid'], row['issn'], row['essn'],\
                          row['pubtype'], row['recordstatus'], row['pubstatus'], row['fulljournalname'], row['sortfirstauthor'],\
-                         row['sortpubdate'], row['vernaculartitle'])
+                         row['sortpubdate'], row['vernaculartitle'], row['medline_ad'], row['abstract'])
 
             cursor.execute(sql, sql_data)
         connection.commit()
-    except:
-        connection.close()
+    except Exception as e:
+        print 'error while inserting: '
+        print e
+        import pdb;pdb.set_trace()
+        PrintException()
+        return
+        # connection.close()
+
+
+def insert_uid_category(uid, category):
+    try:
+        with connection.cursor() as cursor:
+            # Create a new record
+            sql = "INSERT INTO `category_uid` (`uid`, `category`) " + \
+                    "VALUES (%s, %s)"
+            sql_data = ( uid, category)
+
+            cursor.execute(sql, sql_data)
+        connection.commit()
+    except Exception as e:
+        print 'error while insert_uid_category: '
+        print e
+        return
+
+
+def get_medline_ad(uid):
+    url = 'https://www.ncbi.nlm.nih.gov/pubmed/' + uid + '?report=medline'
+    response = urllib.urlopen(url)
+    medline_uid = response.read()
+    import pdb;pdb.set_trace()
+    if 'AD  - ' in medline_uid:
+        return 'Has AD'
+    return 'No AD'
 
 
 def query_one_paper(uids, category):
     # print 'inserting : '
     # print uids
+
     combined_uids = ','.join(uids)
     one_paper_url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&retmode=json&rettype=abstract&id='
     response = urllib.urlopen(one_paper_url+combined_uids)
@@ -45,12 +93,12 @@ def query_one_paper(uids, category):
 
     paper_json = json.loads(paper)
 
-
     for uid in uids:
         try:
-
             if 'result' not in paper_json or uid not in paper_json['result']:
                 return
+            insert_uid_category(uid, category)
+
             cur_paper = paper_json['result'][uid]
 
             row = {}
@@ -59,34 +107,37 @@ def query_one_paper(uids, category):
             row['uid'] = ''
             if 'uid' in cur_paper:
                 try:
-                    row['uid'] = str(cur_paper['uid'])
+                    row['uid'] = unidecode(unidecode(cur_paper['uid']))
                 except:
                     row['uid'] = ''
 
             # category
-            row['category'] = str(category)
+            row['category'] = unidecode(category)
 
             # pubdate
             row['pubdate'] = ''
             if 'pubdate' in cur_paper:
-                row['pubdate'] = str(cur_paper['pubdate'])
+                row['pubdate'] = unidecode(cur_paper['pubdate'])
 
             # epubdate
             row['epubdate'] = ''
             if 'epubdate' in cur_paper:
-                row['epubdate'] = str(cur_paper['epubdate'])
+                row['epubdate'] = unidecode(cur_paper['epubdate'])
 
             # source
             row['source'] = ''
             if 'source' in cur_paper:
-                row['source'] = str(cur_paper['source'])
+                row['source'] = unidecode(cur_paper['source'])
 
             # authors
             row['authors'] = ''
             if 'authors' in cur_paper and len(cur_paper['authors']) > 0:
                 try:
                     authors = cur_paper['authors']
-                    row['authors'] = ','.join(str(x['name']) for x in authors)
+                    row['authors'] = ','.join(unidecode(x['name']) for x in authors)
+                    if len(row['authors']) > 1023:
+                        row['authors'] = row['authors'][:1023]
+                    # row = row[:1023]
                 except:
                     row['authors'] = ''
 
@@ -94,105 +145,113 @@ def query_one_paper(uids, category):
             row['lastauthor'] = ''
             if 'lastauthor' in cur_paper:
                 try:
-                    row['lastauthor'] = str(cur_paper['lastauthor'])
+                    row['lastauthor'] = unidecode(cur_paper['lastauthor'])
                 except:
                     row['lastauthor'] = ''
 
             # title
             row['title'] = ''
             if 'title' in cur_paper:
-                row['title'] = str(cur_paper['title'])
+                row['title'] = unidecode(cur_paper['title'].encode('ascii', 'ignore'))
 
             # sorttitle
             row['sorttitle'] = ''
             if 'sorttitle' in cur_paper:
-                row['sorttitle'] = str(cur_paper['sorttitle'])
+                row['sorttitle'] = unidecode(cur_paper['sorttitle'])
 
             # volume
             row['volume'] = ''
             if 'volume' in cur_paper:
-                row['volume'] = str(cur_paper['volume'])
+                row['volume'] = unidecode(cur_paper['volume'])
 
             # issue
             row['issue'] = ''
             if 'issue' in cur_paper:
-                row['issue'] = str(cur_paper['issue'])
+                row['issue'] = unidecode(cur_paper['issue'])
 
             # pages
             row['pages'] = ''
             if 'pages' in cur_paper:
-                row['pages'] = str(cur_paper['pages'])
+                row['pages'] = unidecode(cur_paper['pages'])
 
             # lang
             row['lang'] = ''
             if 'lang' in cur_paper and len(cur_paper['lang']) > 0:
                 langs = cur_paper['lang']
-                row['lang'] = ','.join(str(l) for l in langs)
+                row['lang'] = ','.join(unidecode(l) for l in langs)
 
             # nlmuniqueid
             row['nlmuniqueid'] = ''
             if 'nlmuniqueid' in cur_paper:
-                row['nlmuniqueid'] = str(cur_paper['nlmuniqueid'])
+                row['nlmuniqueid'] = unidecode(cur_paper['nlmuniqueid'])
 
             # issn
             row['issn'] = ''
             if 'issn' in cur_paper:
-                row['issn'] = str(cur_paper['issn'])
+                row['issn'] = unidecode(cur_paper['issn'])
 
             # essn
             row['essn'] = ''
             if 'essn' in cur_paper:
-                row['essn'] = str(cur_paper['essn'])
+                row['essn'] = unidecode(cur_paper['essn'])
 
             # pubtype
             row['pubtype'] = ''
             if 'pubtype' in cur_paper and len(cur_paper['pubtype']) > 0:
                 pubtype = cur_paper['pubtype']
-                row['pubtype'] = ','.join(str(p) for p in pubtype)
+                row['pubtype'] = ','.join(unidecode(p) for p in pubtype)
 
             # recordstatus
             row['recordstatus'] = ''
             if 'recordstatus' in cur_paper:
-                row['recordstatus'] = str(cur_paper['recordstatus'])
+                row['recordstatus'] = unidecode(cur_paper['recordstatus'])
+                # row = row[:1023]
 
             # pubstatus
             row['pubstatus'] = ''
             if 'pubstatus' in cur_paper:
-                row['pubstatus'] = str(cur_paper['pubstatus'])
+                row['pubstatus'] = unidecode(cur_paper['pubstatus'])
 
             # doi
             row['doi'] = ''
             if 'articleids' in cur_paper:
                 for elem in cur_paper['articleids']:
                     if 'idtype' in elem and elem['idtype'] == 'doi' and 'value' in elem:
-                        row['doi'] = str(elem['value'])
+                        row['doi'] = unidecode(elem['value'])
 
             # fulljournalname
             row['fulljournalname'] = ''
             if 'fulljournalname' in cur_paper:
-                row['fulljournalname'] = str(cur_paper['fulljournalname'])
+                row['fulljournalname'] = unidecode(cur_paper['fulljournalname'])
+                # row = row[:1023]
 
             # sortfirstauthor
             row['sortfirstauthor'] = ''
             if 'sortfirstauthor' in cur_paper:
                 try:
-                    row['sortfirstauthor'] = str(cur_paper['sortfirstauthor'])
+                    row['sortfirstauthor'] = unidecode(cur_paper['sortfirstauthor'])
                 except:
                     row['sortfirstauthor'] = ''
 
             # sortpubdate
             row['sortpubdate'] = '2000/01/01 00:00'
             if 'sortpubdate' in cur_paper:
-                row['sortpubdate'] = str(cur_paper['sortpubdate'])
+                row['sortpubdate'] = unidecode(cur_paper['sortpubdate'])
 
             # vernaculartitle
             row['vernaculartitle'] = ''
             if 'vernaculartitle' in cur_paper:
-                row['vernaculartitle'] = str(cur_paper['vernaculartitle'])
+                row['vernaculartitle'] = unidecode(cur_paper['vernaculartitle'])
+
+
+            row['medline_ad'] = get_medline_ad(uid)
 
             insert_to_db(row)
         except Exception as e:
+            print 'error while building row'
             print e.message
+            # import pdb; pdb.set_trace()
+            PrintException()
             continue
 
     print 'inserted ', len(uids)
@@ -203,31 +262,24 @@ with open('all_count_all_year.txt','w') as out_fp:
     with open('CDE.txt') as fp:
         for line in fp:
             try:
-                # print 'current category line: ',
-                # print line
-                # print line_cnt
                 pieces = line.split(';')
                 if len(pieces) != 2:
                     continue
-                # print pieces
                 category = pieces[1].strip()
-                # print 'category:[', category,']'
-                # print 'cateory name: ', pieces[0]
-                url = url_prefix + pieces[0] + '&retmode=json'
+                url = url_prefix + pieces[0] + '+2015:2016[pdat]' + '&retmode=json'
                 print 'url: ', url
                 response = urllib.urlopen(url)
                 metadata = response.read()
                 metadata = json.loads(metadata)
 
                 cur_total_count = metadata['esearchresult']['count']
-                # print 'cur_total_count: ', cur_total_count
-                url = url_prefix + pieces[0] + '&retmode=json' + '&retmax=' + cur_total_count
+                print category, " + ", cur_total_count
+                url = url_prefix + pieces[0] + '+2015:2016[pdat]' + '&retmode=json' + '&retmax=' + cur_total_count
                 response = urllib.urlopen(url)
                 metadata = response.read()
                 metadata = json.loads(metadata)
 
                 idlist = metadata['esearchresult']['idlist']
-                # print 'len: ', len(idlist)
                 step_size = 500
                 start_idx = 0
                 while start_idx < len(idlist):
@@ -236,10 +288,12 @@ with open('all_count_all_year.txt','w') as out_fp:
                     start_idx = end
                     query_one_paper(uids, category)
 
+
                 to_write = line.rstrip() + ';'+cur_total_count+'\n'
                 total_count += int(cur_total_count)
                 out_fp.write(to_write)
                 line_cnt += 1
+
             except:
                 continue
 print 'total_count: ', total_count
